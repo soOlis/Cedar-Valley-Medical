baf8720580938a50fdf56de85d99aaee5e631950
zulqarnain-ali606 <zulqarnain.ali@bvlogic.com>
2023-08-07 09:48:51 +0500

    configured twilio

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,11 @@
 const router = require("express").Router();
 const testRoutes = require("./test");
 const appointment = require("./setAppointment");
-
+const sendSMS = require("./sendSms");
 
 //Test route
 router.use("/test-route", testRoutes);
 router.use("/api", appointment);
+router.use("/webhook", sendSMS);
 
 module.exports = router;
